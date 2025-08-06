a39f8060b76acba58bec02f3df33f14b1261067a
serbravo38 <sergio3106@gmail.com>
2025-08-06 15:44:29 -0400

    se agrega loqica para conectar con sap

--- models/importaciones.py
+++ models/importaciones.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import configparser
+from pyrfc import Connection  # Instalar paquete pip pyrfc  y el SDK SAP configurado
 from models.inf_importaciones import InfImportaciones
 
 class Importaciones:
@@ -7,17 +9,46 @@ class Importaciones:
 
     def fnc_importaciones_sap(self, desde: str, hasta: str):
         cls_inf_importaciones = InfImportaciones()
+        bol_cierra = False
+
         try:
-            # MOCK de datos para pruebas; reemplaza por pyRFC cuando pase a SAP real
-            cls_inf_importaciones.dtZImportaciones_Cab = pd.DataFrame([
-                {"EBELN": "45000001", "BEDAT": "20240601", "LIFNR": "P001", "NAME1": "Proveedor X", "DENOMINACION": "ASIA", "ZZFECHALLE": "20240610", "WAERS": "USD", "RLWRT": 1000, "FLIBERACION": "20240611", "WKURS": 900, "EKORG": "4000", "ZZPTOEMBA": "PT1", "ERNAM": "JORGE"},
-            ])
-            cls_inf_importaciones.dtZImportaciones_Det = pd.DataFrame([
-                {"EBELN": "45000001", "EBELP": "00010", "TXZ01": "Material X", "MATNR": "M001", "MAKTX": "Producto Y", "WERKS": "PL01", "LGORT": "A01", "MENGE": 10, "MEINS": "PC", "NETPR": 20, "NETWR": 200}
-            ])
-            cls_inf_importaciones.cod_error = 0
-            cls_inf_importaciones.des_error = ''
+            if self.dbc_SAP is None:
+                self.dbc_SAP = self.abrir_sap()
+                bol_cierra = True
+
+            # Llama a la función RFC ZIMPORTACIONES con los parámetros requeridos
+            rfc_response = self.dbc_SAP.call(
+                'ZIMPORTACIONES',
+                FECHADESDE=desde,
+                FECHAHASTA=hasta
+            )
+
+            cls_inf_importaciones.dtZImportaciones_Cab = pd.DataFrame(rfc_response.get('ZIMPORTACION_CAB', []))
+            cls_inf_importaciones.dtZImportaciones_Det = pd.DataFrame(rfc_response.get('ZIMPORTACION_DET', []))
+            cls_inf_importaciones.cod_error = rfc_response.get('CODERROR', 0)
+            cls_inf_importaciones.des_error = ""
+
         except Exception as ex:
-            cls_inf_importaciones.cod_error = 999
+            cls_inf_importaciones.cod_error = 1
             cls_inf_importaciones.des_error = str(ex)
+        finally:
+            if bol_cierra and self.dbc_SAP is not None:
+                self.dbc_SAP.close()
+                self.dbc_SAP = None
+
         return cls_inf_importaciones
+
+    def abrir_sap(self):
+        # Lee los datos de SAP del config.ini
+        config = configparser.ConfigParser()
+        config.read('config.ini')
+        sap_cfg = config['SAP']
+        params = {
+            'user': sap_cfg['user'],
+            'passwd': sap_cfg['passwd'],
+            'ashost': sap_cfg['ashost'],
+            'sysnr': sap_cfg['sysnr'],
+            'client': sap_cfg['client'],
+            'lang': sap_cfg.get('lang', 'ES')
+        }
+        return Connection(**params)
